e8e2a5d17e9cd264402bfea40ec4a49593201d3e
marcdiazzz <diazmarcerwin26@gmail.com>
2020-06-02 21:42:20 +0800

    third commit

--- src/App.js
+++ src/App.js
@@ -1,33 +1,77 @@
-import React,{Component} from 'react';
+import React,{Component, Fragment} from 'react';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import './App.css';
 import NavBar from './components/layout/NavBar';
+import Alert from './components/layout/Alert';
 import Users from './components/users/Users';
+import User from './components/users/User';
 import Search from './components/users/Search';
+import About from './components/pages/About';
 import axios from 'axios';
 class App extends Component{
   //Render - is a life-cycle method, 
   state = {
     users:[],
-    loading: false
+    user:{},
+    loading: false,
+    repos: [],
+    alert: null
   };
-  async componentDidMount(){
-    console.log(process.env.REACT_APP_GITHUB_CLIENT_SECRET);
+  
+  //Search GitHub Users
+  searchUsers = async (text) =>{
     this.setState({loading: true});
-    const res = await axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
-    this.setState({users: res.data, loading:false});
+    const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+    this.setState({users: res.data.items, loading:false});
+  }
+  //Get Single github user
+  getUser = async(username) => {
+    this.setState({loading: true});
+    const res = await axios.get(`https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+    this.setState({user: res.data, loading:false});
+  }
+  //GetUser Repos
+  getUserRepos= async(username) => {
+    this.setState({loading: true});
+    const res = await axios.get(`https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+    this.setState({repos: res.data, loading:false});
   }
 
+  //Clear Users
+  clearUsers  = () => this.setState({users: [], loading: false});
+
+  //Alert
+  setAlert = (msg, type) =>{
+    this.setState({alert:{msg,type}});
+
+    setTimeout(() => this.setState({alert:null}),5000);
+  } 
+
   render(){
+    const {users , loading , user , repos} = this.state;
     return(
-      //JSX
+    <Router>
      <div className="App">
      <NavBar/>
      <div className='container'>
-     <Search />
-     <Users loading={this.state.loading} users={this.state.users}/>
+     <Alert alert={this.state.alert}/>
+      <Switch>
+        <Route exact path='/'  render={props => (
+          <Fragment>
+            <Search searchUsers={this.searchUsers} 
+            clearUsers={this.clearUsers} 
+            showClear={users.length > 0 ? true : false} setAlert={this.setAlert}/>
+            <Users loading={loading} users={users}/>
+          </Fragment>
+        )} />
+        <Route exact path='/about' component={About}/>
+         <Route exact path='/user/:login' render={props => (
+           <User {...props } getUser={this.getUser} getUserRepos={this.getUserRepos} user={user} repos={repos} loading={loading} />
+         )} /> 
+      </Switch>
      </div>
      </div>
-  
+    </Router> 
   
   );
 }
--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types'
+import {Link} from 'react-router-dom';
 const UserItem = ({user: {login , avatar_url , html_url}}) =>  {
         return (
             <div className='card text-center'>
@@ -9,9 +10,9 @@ const UserItem = ({user: {login , avatar_url , html_url}}) =>  {
                 <h3>{login}</h3>
                 </div>
                 <div>
-                    <a href={html_url} className='btn btn-dark btn-sm ny-1'>
+                    <Link to={`/user/${login}`} className='btn btn-dark btn-sm ny-1'>
                         More
-                    </a>
+                    </Link>
                 </div>
                 
             </div>
